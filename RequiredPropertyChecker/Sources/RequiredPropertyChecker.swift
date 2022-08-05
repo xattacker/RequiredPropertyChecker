a4e0527d9ff13f5998c67f0bb4446461173c8650
xattacker.tao <xattacker.tao@car-plus.com.tw>
2022-08-05 09:54:19 +0800

    update RequiredPropertyCheckMode

--- RequiredPropertyChecker/Sources/RequiredPropertyChecker.swift
+++ RequiredPropertyChecker/Sources/RequiredPropertyChecker.swift
@@ -18,8 +18,8 @@ public enum RequiredPropertyCheckMode
     /// 全都要填
     case all
     
-    /// 只要有一個有填
-    case contained
+    /// 只要有一定數量有填
+    case contained(count: Int)
 }
 
 
@@ -67,8 +67,9 @@ public final class RequiredPropertyChecker: ReactiveCompatible
                     }
                     break
                 
-                case .contained:
-                    return self.properties.first(where: { $0.property != nil && $0.property.isRequired && $0.property.isFilled }) != nil
+                case .contained(let count):
+                    let filled = self.properties.filter { $0.property != nil && $0.property.isRequired && $0.property.isFilled }
+                    return filled.count >= count
             }
             
             return true
