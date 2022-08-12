03d31f68711ca07a95cba79499beb83f4a2a8498
xattacker.tao <xattacker.tao@car-plus.com.tw>
2022-08-12 11:13:07 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,27 +4,22 @@
 import PackageDescription
 
 let package = Package(
-    name: "RxRequiredPropertyChecker",
-    platforms: [.iOS(.v10)],
+    name: "RequiredPropertyChecker",
+    platforms: [.iOS(.v13)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "RxRequiredPropertyChecker",
-            targets: ["RxRequiredPropertyChecker"]),
+            name: "RequiredPropertyChecker",
+            targets: ["RequiredPropertyChecker"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-       .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "6.2.0")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "RxRequiredPropertyChecker",
-            dependencies: [.product(name: "RxSwift", package: "RxSwift"),
-                          .product(name: "RxCocoa", package: "RxSwift")
-                          ],
- 	    path: "RxRequiredPropertyChecker/Sources"),
+            name: "RequiredPropertyChecker",
+ 	    path: "RequiredPropertyChecker/Sources"),
     ],
     swiftLanguageVersions: [.v5]
 )
