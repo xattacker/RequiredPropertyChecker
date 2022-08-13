4ea4668d98532c61664563552b23e4b2999a1743
Yu Lin Tao <amigo.xattacker@gmail.com>
2022-08-14 01:29:00 +0800

    Update RequiredPropertyChecker.swift

--- RequiredPropertyChecker/Sources/RequiredPropertyChecker.swift
+++ RequiredPropertyChecker/Sources/RequiredPropertyChecker.swift
@@ -70,7 +70,7 @@ public final class RequiredPropertyChecker
                 
                 case .contained(let count):
                     let filled = self.properties.filter { $0.property != nil && $0.property.isRequired && $0.property.isFilled }
-                    return filled.count >= (self.properties > count ? count : self.properties)
+                    return filled.count >= (self.properties.count > count ? count : self.properties.count)
             }
             
             return true
